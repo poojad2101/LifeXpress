7e51aa87037b23142e74dfd050a85cd5d5148a7a
poojad2101 <davepooja2101@gmail.com>
2022-07-24 18:56:07 +0530

    new files added to create a topbar along with some css and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import TopBar from "./topbar/TopBar";
 
 function App() {
   return (
-    <div className="App">
-     blog app
-      
-    </div>
+    <>
+    <TopBar/>
+    </>
   );
 }
-
 export default App;
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,18 @@
+import "./TopBar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+
+        </div>
+        <div className="topCenter">b</div>
+        <div className="topRight">c</div>
+    </div>
+
+  )
+}
