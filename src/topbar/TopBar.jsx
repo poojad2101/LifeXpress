c9adb15673f823490a48adef7124cbcde4f29fa3
poojad2101 <davepooja2101@gmail.com>
2022-07-24 20:36:44 +0530

    topbar now has all required icons and  profile picture

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -10,9 +10,25 @@ export default function TopBar() {
         <i className="topIcon fab fa-twitter-square"></i>
 
         </div>
-        <div className="topCenter">b</div>
-        <div className="topRight">c</div>
-    </div>
-
-  )
+        <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+        </div>
+        <div className="topRight">
+            <img 
+              className="topImg"
+              src="https://bestprofilepictures.com/wp-content/uploads/2021/04/Profile-Pic-For-Girl.jpg" 
+              alt="" 
+              />
+              <i className="topSearchIcon fas fa-search"></i>
+              </div>
+            
+            </div>  
+            
+           )
 }
