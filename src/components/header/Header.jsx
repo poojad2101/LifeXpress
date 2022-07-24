95ee049622770155f50ccc7967401b81ec1f9410
poojad2101 <davepooja2101@gmail.com>
2022-07-24 22:00:08 +0530

    working on homepage and header components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleLg">LifeXpress!</span>
+        </div>
+        <img className="headerImg" src="https://i0.wp.com/knowledgeenthusiast.com/wp-content/uploads/2020/04/pexels-photo-2379653.jpeg?fit=1880%2C1253&ssl=1" alt="" />
+        </div>
+  )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/header/Header";
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <div className="home">
+        
+      </div>
+    </>
+  );
+}
