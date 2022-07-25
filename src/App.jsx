81f0e168a9b92a0b4b18dc355cb0e56081e64edc
poojad2101 <davepooja2101@gmail.com>
2022-07-25 22:03:28 +0530

    created files and folders for settings page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import Topbar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
     {/* <Home /> */}
     <Topbar />
-    <Write />
+    <Settings />
     </>
   );
 }
