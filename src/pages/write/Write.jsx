c4376881f87af990fb6b724600e07b3a0d94aad3
poojad2101 <davepooja2101@gmail.com>
2022-07-25 21:34:34 +0530

    setup write post page

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -3,6 +3,10 @@ import "./Write.css"
 export default function Write() {
   return (
     <div className="write">
+        <img
+        className="writeImg"
+        src="https://i0.wp.com/knowledgeenthusiast.com/wp-content/uploads/2020/04/pexels-photo-2379653.jpeg?fit=1880%2C1253&ssl=1" 
+        alt="" />
         <form className="writeForm">
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
