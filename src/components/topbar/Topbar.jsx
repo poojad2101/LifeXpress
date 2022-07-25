791255c041afc8ab1b8b110ef7c8389b9d87007e
poojad2101 <davepooja2101@gmail.com>
2022-07-26 01:23:15 +0530

    all routes working to various links

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,33 +1,58 @@
+import { Link } from "react-router-dom";
 import "./Topbar.css";
 
 export default function Topbar() {
+  const user = false;
   return (
     <div className="top">
-        <div className="topLeft">
+      <div className="topLeft">
         <i className="topIcon fab fa-facebook-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
         <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-twitter-square"></i>
-        </div>
-        <div className="topCenter">
+      </div>
+      <div className="topCenter">
         <ul className="topList">
-          <li className="topListItem">HOME</li>
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
           <li className="topListItem">ABOUT</li>
           <li className="topListItem">CONTACT</li>
-          <li className="topListItem">WRITE</li>
-          <li className="topListItem">LOGOUT</li>
-        </ul> 
-        </div>
-        <div className="topRight">
-            <img 
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
               className="topImg"
-              src="https://bestprofilepictures.com/wp-content/uploads/2021/04/Profile-Pic-For-Girl.jpg" 
-              alt="" 
-              />
-              <i className="topSearchIcon fas fa-search"></i>
-              </div>
-            
-            </div>  
-            
-           )
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
+    </div>
+  );
 }
