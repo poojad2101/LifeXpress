18ed4663f21fbc8761a5ac546d951c73939ab5ce
poojad2101 <davepooja2101@gmail.com>
2022-07-25 22:30:55 +0530

    made a login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
     {/* <Home /> */}
     <Topbar />
-    <Settings />
+    <Login />
     </>
   );
 }
