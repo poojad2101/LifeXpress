7d4d8053aefd5c07a5cd4775aa6d0ad1d204d13f
poojad2101 <davepooja2101@gmail.com>
2022-07-26 00:38:58 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,23 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = false;
   return (
-    <>
-    {/* <Home /> */}
-    <Topbar />
-    <Register />
-    </>
+    <Router>
+      <Topbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
