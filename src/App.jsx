ee4b90de9cb46432afe44c9bc7db5f7f2f1a35d4
poojad2101 <davepooja2101@gmail.com>
2022-07-25 14:03:31 +0530

    new files and folder for creating a blog post page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Topbar from "./components/topbar/TopBar";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
     {/* <Home /> */}
     <Topbar />
-    <Single />
+    <Write />
     </>
   );
 }
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,10 @@
+import "./Write.css"
+
+export default function Write() {
+  return (
+    <div className="write">
+        abc
+      
+    </div>
+  )
+}
