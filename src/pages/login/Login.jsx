fcf6cd925759deffa850a0ebfeb8414cff267293
poojad2101 <davepooja2101@gmail.com>
2022-07-27 08:31:38 +0530

    installed dependencies

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,9 @@
-import "./Login.css"
+import "./Login.css";
 
 export default function Login() {
   return (
     <div className="login">
-        <span className="loginTitle">Login</span>
+      <span className="loginTitle">Login</span>
       <form className="loginForm">
         <label>Email</label>
         <input className="loginInput" type="text" placeholder="Enter your email..." />
