40156be0f9b9b57b0692e27d7953b087c55184f6
poojad2101 <davepooja2101@gmail.com>
2022-07-27 09:59:31 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Topbar from "./components/topbar/TopBar";
+import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
