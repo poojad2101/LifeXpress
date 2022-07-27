16de85da7446c71060e3bf05814e1d3e88019d1b
poojad2101 <davepooja2101@gmail.com>
2022-07-28 00:07:55 +0530

    installed bycrypt dependency and setup rputes for register and login

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -13,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
    .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+  app.use("routes/auth", authRoute);
 
 app.use("/",(req, res) => {
 console.log("hi this is my app")
