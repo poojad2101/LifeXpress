a39c2790afbd29f96f2c8a25bf9d922abca0203d
poojad2101 <davepooja2101@gmail.com>
2022-07-28 10:33:54 +0530

    delete and get post

--- routes/posts.js
+++ routes/posts.js
@@ -42,13 +42,30 @@ const Post = require("../models/Post");
   
 
   //delete post
+  router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.username === req.body.username) {
+            try {
+                await post.delete()
+            res.status(200).json("Post has been deleted");
+          } catch (err) {
+            res.status(500).json(err);
+          }
+        } else {
+          res.status(401).json("You can delete only your post!");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    })
 
   //get post
   router.get("/:id", async (req, res) => {
     try{
         const user = await user.findById(req.param.id);
         const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
   }
