0a623733cbc22867ded6e2223b40b3c848d7e316
poojad2101 <davepooja2101@gmail.com>
2022-07-28 10:43:43 +0530

    get router for all posts testing..trying to bug fix

--- routes/posts.js
+++ routes/posts.js
@@ -71,6 +71,19 @@ const Post = require("../models/Post");
   }
 });
 
+//get all posts
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const category = req.query.user;
+    try {
+        const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 
 
