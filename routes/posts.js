aa5d47d55400b1b5fe3daedd91f9f2dab3f7d691
poojad2101 <davepooja2101@gmail.com>
2022-07-28 10:46:44 +0530

    get route works for posts

--- routes/posts.js
+++ routes/posts.js
@@ -74,9 +74,21 @@ const Post = require("../models/Post");
 //get all posts
 router.get("/", async (req, res) => {
     const username = req.query.user;
-    const category = req.query.user;
+    const categoryName = req.query.user;
     try {
-        const post = await Post.findById(req.params.id);
+        let posts;
+        if (username) {
+            posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+        
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
